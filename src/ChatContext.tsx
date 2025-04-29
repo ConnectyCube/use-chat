f0fb336332d9ad1a530957d8b59cbda3c046d328
ConnectyCube <admin@connectycube.com>
2025-04-29 10:29:49 +0300

    do not crash when receive typing for non existent chat

--- src/ChatContext.tsx
+++ src/ChatContext.tsx
@@ -538,8 +538,8 @@ export const ChatProvider = ({ children }: ChatProviderType): React.ReactElement
 
   const _clearTypingStatus = (dialogId: string, userId: number) => {
     _updateTypingStatus(dialogId, userId, false);
-    clearTimeout(typingTimers.current[dialogId][userId]);
-    delete typingTimers.current[dialogId][userId];
+    clearTimeout(typingTimers.current[dialogId]?.[userId]);
+    delete typingTimers.current[dialogId]?.[userId];
   };
 
   const _getPrivateDialogIdByUserId = (userId: number): string | undefined => {
@@ -784,8 +784,8 @@ export const ChatProvider = ({ children }: ChatProviderType): React.ReactElement
     if (isTyping) {
       // clear previous and run new timer
       if (typingTimers.current[_dialogId]?.[userId]) {
-        clearTimeout(typingTimers.current[_dialogId][userId]);
-        delete typingTimers.current[_dialogId][userId];
+        clearTimeout(typingTimers.current[_dialogId]?.[userId]);
+        delete typingTimers.current[_dialogId]?.[userId];
       }
       typingTimers.current[_dialogId][userId] = setTimeout(() => {
         _clearTypingStatus(_dialogId, userId);
